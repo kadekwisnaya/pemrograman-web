7d43f073dde0499418702e1b28033b5c3b303b7f
kadekwisnaya <167794641+kadekwisnaya@users.noreply.github.com>
2024-04-23 10:32:40 +0700

    Add files via upload

---
+++ dekwis/array1.php
@@ -0,0 +1,14 @@
+<?php
+$kelompok1 = array("Andi", "Budi", "Chika", "David", "Erna");
+echo "Nama Mahasiswa Kelompok 1 :<br />";
+foreach ($kelompok1 as $nama) {
+    echo " - ", $nama, "<br/>";
+}
+echo "<br /><br />";
+
+$kelompok2 = array("Fera", "Gani", "Hery", "Intan", "Jaka");
+echo "Nama Mahasiswa Kelompok 2 :<br />";
+foreach ($kelompok2 as $nama) {
+    echo " - ", $nama, "<br/>";
+}
+?>
